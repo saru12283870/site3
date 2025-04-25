a835ce9e83ec5942e8459a87d01069c4d513a61e
saru12283870 <rui201412283870@gmail.com>
2025-04-25 16:48:26 +0900

    .css

--- public/js/webshell.js
+++ public/js/webshell.js
@@ -1,5 +1,11 @@
 async function main() {
-    console.log(await readline("WS >"));
+    await readline(`WS  ${localStorage.cd==undefined?"":localStorage.cd}>`)
+    if (localStorage.fd==undefined) {
+        localStorage.fd=await openFile()
+        localStorage.cd=fd.name
+    }
+    console.log();
+
     await main()
 }
 main()
